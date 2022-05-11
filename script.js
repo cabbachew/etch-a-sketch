4ac6bfe8804f9a00ec308ae974c6580675e91dff
Daniel Oh <cabbachew@gmail.com>
2022-05-11 15:27:08 +0900

    Clear grid of divs when grid is resized

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function populateGrid(size) {
   let grid = document.querySelector(".grid");
+  let pixels = grid.querySelectorAll("div");
+  pixels.forEach((div) => div.remove()); // remove all existing divs within grid
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < size ** 2; i++) {
+  for (let i = 0; i < size * size; i++) {
     let pixel = document.createElement("div");
     pixel.style.backgroundColor = "#CCC";
     grid.insertAdjacentElement("beforeend", pixel);
